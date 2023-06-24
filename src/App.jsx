3a5a57a1e1ac6be919cd85f51db6bda467e088a0
akash <akashsah.aks@gmail.com>
2023-06-24 19:33:36 +0530

    setup google font for whole html and layer directive for specific use

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <h1 className="text-4xl text-center font-signature">Akash</h1>
+      <h2 className="text-3xl font-bold underline">Hello world!</h2>;
     </>
   );
 }
