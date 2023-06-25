5be72468b53ba0726e2995d0ac5833a4205fb5df
akash <akashsah.aks@gmail.com>
2023-06-25 23:06:11 +0530

    contact form done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Skill from "./components/Skill";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <About />
       <Portfolio />
       <Skill />
+      <Contact />
     </>
   );
 }
